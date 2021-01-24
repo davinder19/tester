9cf32bfb798ff0e6701734fc07d85f4b0a0962db
davinder19 <48900931+davinder19@users.noreply.github.com>
2021-01-24 07:52:25 -0800

    Add files via upload
    tester

---
+++ nav.js
@@ -0,0 +1,118 @@
+
+//////////////////////////////////NavBar///////////////////////////////
+const nav = document.querySelector('.nav')
+window.addEventListener('scroll', fixNav)
+
+function fixNav() {
+    if(window.scrollY > nav.offsetHeight + 150) {
+        nav.classList.add('act')
+    } else {
+        nav.classList.remove('act')
+    }
+}
+
+////////////////////////Words Counting///////////////////////
+var header = document.getElementById("parents");
+// var btns = document.getElementsByClassName("child");
+var btns = document.getElementsByClassName("fa");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace("active", "");
+  this.className += " active";
+  });
+}
+/////////////////////////////////Typing Effect///////////////////////////////////////
+class TypeWriter {
+  constructor(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait, 10);
+    this.type();
+    this.isDeleting = false;
+  }
+
+  type() {
+    // Current index of word
+    const current = this.wordIndex % this.words.length;
+    // Get full text of current word
+    const fullTxt = this.words[current];
+
+    // Check if deleting
+    if (this.isDeleting) {
+      // Remove char
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      // Add char
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    // Insert txt into element
+    this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+
+    // Initial Type Speed
+    let typeSpeed = 300;
+
+    if (this.isDeleting) {
+      typeSpeed /= 2;
+    }
+
+    // If word is complete
+    if (!this.isDeleting && this.txt === fullTxt) {
+      // Make pause at end
+      typeSpeed = this.wait;
+      // Set delete to true
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      // Move to next word
+      this.wordIndex++;
+      // Pause before start typing
+      typeSpeed = 500;
+    }
+
+    setTimeout(() => this.type(), typeSpeed);
+  }
+}
+
+// Init On DOM Load
+document.addEventListener('DOMContentLoaded', init);
+
+// Init App
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+  // Init TypeWriter
+  new TypeWriter(txtElement, words, wait);
+}
+
+////////////////////////////////Date-Time////////////////////////////
+// const hourEl = document.querySelector('.hour')
+// const minuteEl = document.querySelector('.minute')
+// const secondEl = document.querySelector('.second')
+const timeEl = document.querySelector('.time')
+const dateEl = document.querySelector('.date')
+
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thrusday", "Friday", "Saturday", "Sunday"];
+const months = ["Jan", "Fab", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+function setTime() {
+  const time = new Date();
+  const month = time.getMonth()
+  const day = time.getDay()
+  const date = time.getDate()
+  const hours = time.getHours()
+  const hoursForClock = hours >= 13 ? hours % 12 : hours;
+  const minutes = time.getMinutes()
+  const seconds = time.getSeconds()
+  const ampm = hours >= 12 ? 'PM' : 'AM'
+
+  // timeEl.innerHTML =`${hoursForClock}`
+  timeEl.innerHTML = `${hoursForClock}:${minutes < 10 ? `0${minutes}` : minutes} ${ampm}`
+  dateEl.innerHTML = `${days[day]}, ${months[month]} <span class="circle">${date}</span>`
+  // setInterval(setTime, 1000)
+}
+setTime()
